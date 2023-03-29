33b3f3283e65774b747c131d614ccd408ff16681
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-03-29 14:49:51 +0100

    sidebar component created

--- whatsapp-mern/src/Sidebar.js
+++ whatsapp-mern/src/Sidebar.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import './Sidebar.css';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import { Avatar, IconButton } from '@mui/material';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { SearchOutlined } from '@mui/icons-material';
 
 function Sidebar() {
     return (
-        <div className='sidebar'>Sidebar
+        <div className='sidebar'>
             <div className='sidebar__header'>
-
+                <Avatar src='https://ksassets.timeincuk.net/wp/uploads/sites/55/2017/12/mrburns.png' /> 
+                <div className='sidebar__headerRight'>
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
             </div>
+
+            <div className='sidebar__search'>
+                <div className='sidebar__searchContainer'>
+                    <SearchOutlined />
+                    <input placeholder='Search or start new chat' type='text' />
+                </div>
+            </div>
+
         </div>
     
     )
